307992e4efe54c13e890445dd41369a4817863b5
Alex Slaughter <alex@alexslaughter.com>
2018-05-21 03:19:16 -0400

    fix spacing

--- src/Query/Builder.php
+++ src/Query/Builder.php
@@ -1,17 +1,10 @@
 <?php namespace Cubettech\Lacassa\Query;
 
-use Closure;
-use DateTime;
 use InvalidArgumentException;
 use Illuminate\Database\Query\Builder as BaseBuilder;
-use Illuminate\Database\Query\Expression;
 use Illuminate\Support\Arr;
-use Illuminate\Support\Collection;
 use Cubettech\Lacassa\Connection;
 use Cassandra;
-
-
-
 
 class Builder extends BaseBuilder
 {
@@ -189,7 +182,7 @@ class Builder extends BaseBuilder
      */
     public function get($columns = ['*'])
     {
-        if(is_null($this->columns)) {
+        if (is_null($this->columns)) {
             $this->columns = $columns;
         }
         $cql = $this->toCql();
@@ -206,8 +199,7 @@ class Builder extends BaseBuilder
      */
     public function bindQuery($cql)
     {
-        foreach($this->getBindings() as $binding)
-        {
+        foreach ($this->getBindings() as $binding) {
             $value = is_numeric($binding) ? $binding : "'".$binding."'";
             $cql = preg_replace('/\?/', $value, $cql, 1);
         }
@@ -295,10 +287,9 @@ class Builder extends BaseBuilder
         // Here we will make some assumptions about the operator. If only 2 values are
         // passed to the method, we will assume that the operator is an equals sign
         // and keep going. Otherwise, we'll require the operator to be passed in.
-        if(func_num_args() == 3)
-        {
-          $value = $operation;
-          $operation = null;
+        if (func_num_args() == 3) {
+            $value = $operation;
+            $operation = null;
         }
         $updateCollection = compact('type', 'column', 'value', 'operation');
         $this->updateCollections[] = $updateCollection;
@@ -320,7 +311,7 @@ class Builder extends BaseBuilder
         if (! array_key_exists($type, $this->bindings)) {
             throw new InvalidArgumentException("Invalid binding type: {$type}.");
         }
-      $this->bindings[$type][] = $value;
+        $this->bindings[$type][] = $value;
         return $this;
     }
 
@@ -330,83 +321,84 @@ class Builder extends BaseBuilder
      * @param  array $values
      * @return int
      */
-        public function update(array $values = [])
-        {
-            $cql = $this->grammar->compileUpdate($this, $values);
-            return $this->connection->update(
-                $cql, $this->cleanBindings(
+    public function update(array $values = [])
+    {
+        $cql = $this->grammar->compileUpdate($this, $values);
+        return $this->connection->update(
+                $cql,
+                $this->cleanBindings(
                     $this->grammar->prepareBindingsForUpdate($this->bindings, $values)
                 )
             );
+    }
+
+    /**
+     * Insert a new record into the database.
+     *
+     * @param  array  $values
+     * @return bool
+     */
+    public function insert(array $values = [])
+    {
+        $insertCollectionArray = [];
+        // Since every insert gets treated like a batch insert, we will make sure the
+        // bindings are structured in a way that is convenient when building these
+        // inserts statements by verifying these elements are actually an array.
+        if (empty($values)) {
+            return true;
         }
 
-      /**
-       * Insert a new record into the database.
-       *
-       * @param  array  $values
-       * @return bool
-       */
-      public function insert(array $values = [])
-      {
-          $insertCollectionArray = [];
-          // Since every insert gets treated like a batch insert, we will make sure the
-          // bindings are structured in a way that is convenient when building these
-          // inserts statements by verifying these elements are actually an array.
-          if (empty($values)) {
-              return true;
-          }
+        if (! is_array(reset($values))) {
+            $values = [$values];
+        }
 
-          if (! is_array(reset($values))) {
-              $values = [$values];
-          }
-
-  				if (! is_array(reset($values))) {
-              $values = [$values];
-          }
+        if (! is_array(reset($values))) {
+            $values = [$values];
+        }
 
 
-          // Here, we will sort the insert keys for every record so that each insert is
-          // in the same order for the record. We need to make sure this is the case
-          // so there are not any errors or problems when inserting these records.
-          else {
-              foreach ($values as $key => $value) {
-                  ksort($value);
-                  $values[$key] = $value;
-              }
-          }
-          // Finally, we will run this query against the database connection and return
-          // the results. We will need to also flatten these bindings before running
-          // the query so they are all in one huge, flattened array for execution.
-          return $this->connection->insert(
+        // Here, we will sort the insert keys for every record so that each insert is
+        // in the same order for the record. We need to make sure this is the case
+        // so there are not any errors or problems when inserting these records.
+        else {
+            foreach ($values as $key => $value) {
+                ksort($value);
+                $values[$key] = $value;
+            }
+        }
+        // Finally, we will run this query against the database connection and return
+        // the results. We will need to also flatten these bindings before running
+        // the query so they are all in one huge, flattened array for execution.
+        return $this->connection->insert(
               $this->grammar->compileInsert($this, $values),
               $this->cleanBindings(Arr::flatten($values, 1))
           );
-      }
+    }
 
-      /**
-       * [insertCollection insert a colletion type in cassandra]
-       * @param  [type] $type   [description]
-       * @param  [type] $column [description]
-       * @param  [type] $value  [description]
-       * @return [type]         [description]
-       */
-      public function insertCollection($type, $column, $value)
-      {
+    /**
+     * [insertCollection insert a colletion type in cassandra]
+     * @param  [type] $type   [description]
+     * @param  [type] $column [description]
+     * @param  [type] $value  [description]
+     * @return [type]         [description]
+     */
+    public function insertCollection($type, $column, $value)
+    {
         $insertCollection = compact('type', 'column', 'value');
         $this->insertCollections[] = $insertCollection;
         $this->addCollectionBinding($insertCollection, 'insertCollection');
         return $this;
-      }
+    }
 
-      /**
-       * [index description]
-       * @param  [type] $columns [description]
-       * @return [type]          [description]
-       */
-      public function index($columns = [])
-      {
+    /**
+     * [index description]
+     * @param  [type] $columns [description]
+     * @return [type]          [description]
+     */
+    public function index($columns = [])
+    {
         $cql = $this->grammar->compileIndex($this, $columns);
         $result = $this->executeCql($cql);
         return $result;
-      }
+    }
 }
